35ed2776f9e162ebdae00128dd1645e445caa692
Michael Centrelli <centm860@newschool.edu>
2017-06-05 11:08:17 -0600

    More tests

--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -17,6 +17,20 @@ class InvoiceItemRepositoryTest < MiniTest::Test
     assert_instance_of InvoiceItem, invoice
   end
 
+  def test_it_can_find_all_by_item_id
+    ir = InvoiceItemRepository.new('./test/data/invoice_items_fixture.csv', sales_engine = nil)
+    invoice = ir.find_all_by_item_id(263542298)
+
+    assert_equal 1, invoice.count
+  end
+
+  def test_it_can_find_all_by_item_id
+    ir = InvoiceItemRepository.new('./test/data/invoice_items_fixture.csv', sales_engine = nil)
+    invoice = ir.find_all_by_invoice_id(2)
+
+    assert_equal 1, invoice.count
+  end
+
 
 
 
