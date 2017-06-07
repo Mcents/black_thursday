7adfffb98b00186c9dcbf096ebc025ea706b3778
Michael Centrelli <centm860@newschool.edu>
2017-06-07 16:51:28 -0600

    More tests

--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -30,29 +30,30 @@ class InvoiceTest < MiniTest::Test
       refute_equal 9, @invoice.customer_id
     end
 
+    def test_it_can_call_on_merchant_id
 
-    # def test_it_can_call_on_unit_price
-    #
-    #   assert_equal BigDecimal.new(10.99,4), @item.unit_price
-    # end
-    #
-    # def test_it_can_call_on_merchant_id
-    #
-    #   assert_equal 12345678, @item.merchant_id
-    # end
-    #
-    # def test_item_has_a_create_date
-    #   created_at = Time.gm(2017, 5, 27, 15, 44, 2)
-    #
-    #   assert_equal created_at, @item.created_at
-    # end
-    #
-    # def test_item_has_an_update_date
-    #   updated_at = Time.gm(2017, 5, 29, 14, 56, 56)
-    #
-    #   assert_equal updated_at, @item.updated_at
-    # end
-    #
+      assert_equal 12335938, @invoice.merchant_id
+      refute_equal 21523555, @invoice.merchant_id
+    end
+
+    def test_it_can_call_on_status
+
+      assert_equal :pending, @invoice.status
+      refute_equal :shipped, @invoice.status
+    end
+
+    def test_item_has_a_create_date
+      created_at = Time.gm(2017, 5, 27, 15, 44, 2)
+
+      assert_equal created_at, @invoice.created_at
+    end
+
+    def test_item_has_an_update_date
+      updated_at = Time.gm(2017, 5, 29, 14, 56, 56)
+
+      assert_equal updated_at, @invoice.updated_at
+    end
+
     # def test_unit_price_to_dollars_returns_a_float
     #   assert_equal @item.unit_price_to_dollars.class, Float
     #   assert_equal @item.unit_price_to_dollars, 10.99
